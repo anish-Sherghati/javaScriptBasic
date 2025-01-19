fd7d53423f9a1c8c3828d0afda52b0bfdc1aec8e
anish-Sherghati <ani.abdullha@gmail.com>
2025-01-19 19:19:55 +0530

    FirstCommit

---
+++ 02_dataTypes.js
@@ -0,0 +1,52 @@
+// "use strict"; // treat all JS code as newer version
+
+// // alert( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3 
+//     +
+//      3) // code readability should be high
+
+// console.log("Hitesh")
+
+
+console.log(3 + 3)
+// alert(3+3); only for browser it is available
+console.log("HItesh");
+
+
+// let name = "hitesh"
+// let age = 18
+// let isLoggedIn = false
+// let state;
+
+let name = "anish";
+let age = 18;
+let isLogged = true;
+let state;
+let xyz =$;
+
+console.table([name, age, isLogged, state]);
+
+// number => 2 to power 53;
+// bigint
+// string => ""
+// boolean => true / false;
+// null => standalone value
+// undefined => 
+// sysmbol =>unique   
+
+
+// // object
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+
+// object
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLogged);
+console.log(typeof state);
+console.log(typeof xyz);
