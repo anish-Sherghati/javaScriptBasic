bcdd6bbd52362072229b0c0e5cd71c07b3702978
anish-Sherghati <ani.abdullha@gmail.com>
2025-01-19 18:43:40 +0530

    Initial commit

---
+++ 01_variables.js
@@ -0,0 +1,52 @@
+// const accountId = 144553
+// let accountEmail = "hitesh@google.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
+// let accountState;
+
+// // accountId = 2 // not allowed
+
+const accountId ="12345";
+console.log(accountId);
+
+let accountEmail =123.45;
+console.log(accountEmail);
+
+accountEmail =123.46;
+console.log(accountEmail);
+
+var accountPassword ="123456";
+console.log(accountPassword);
+
+accountPassword="789";
+console.log(accountPassword);
+
+accountstate ="jaipur";
+console.log(accountstate)
+
+accountstate ="GAYA";
+console.log(accountstate)
+
+console.log("*****************");
+console.table([accountId,accountEmail,accountPassword,accountstate]);
+
+
+
+
+
+
+
+
+// accountEmail = "hc@hc.com"
+// accountPassword = "21212121"
+// accountCity = "Bengaluru"
+
+// console.log(accountId);
+
+// /*
+// Prefer not to use var
+// because of issue in block scope and functional scope
+// */
+
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
